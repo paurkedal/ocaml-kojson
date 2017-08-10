e1c99a71228675b504b8973cc21ee1a48c9e00a7
Petter A. Urkedal <paurkedal@gmail.com>
2017-08-10 21:00:07 +0200

    Move src/ to lib/.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -7,5 +7,5 @@ let licenses = List.map Pkg.std_file ["COPYING.LESSER"; "COPYING"]
 
 let () = Pkg.describe ~licenses "kojson" @@ fun c ->
   Ok [
-    Pkg.mllib ~api:["Kojson"; "Kojson_pattern"] "src/kojson.mllib";
+    Pkg.mllib ~api:["Kojson"; "Kojson_pattern"] "lib/kojson.mllib";
   ]
